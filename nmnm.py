00d31d4bf976d81ee1e2e856df691bb4b82d4ac2
philip-mv <philipmv10@gmail.com>
2025-05-08 16:03:27 +0530

    jbkbk n

---
+++ nmnm.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ",n)
+    iteration+= 1
+    print("Total iterations done by this code is",iteration,"\n")
+
+printnumber(10)
+printnumber(20)
